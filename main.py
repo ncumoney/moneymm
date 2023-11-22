5bb0062560a54c85ecc37e88c23e5d00c221aadd
ffloooot <149374300+ffloooot@users.noreply.github.com>
2023-11-22 23:31:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-
-from flask import Flask, request
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import TextSendMessage   # 載入 TextSendMessage  模組
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import json
+import os
+
+line_bot_api = LineBotApi("IjD9cOGGINHUXelSEl+HdVAc9oEDw3/kk+XMkfWyGZCdFyURygI18eD4rKfcpaKxajwsLmA0iCwnedwrM/qPSCy5BcBNNw+z8xIx/k4ytwxrAABJspIvWUUTWEYZOnYGRUUtw1B9Ez2tyL9qhqWhcwdB04t89/1O/w1cDnyilFU=")
+line_handler = WebhookHandler("6e4d6c59b5cd885348d5e5cc71a4957b")
 
 app = Flask(__name__)
 
@@ -12,6 +16,30 @@ def home():
 
 @app.route("/webhook", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.type != "text":
+        return
+
+    line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
+    return
+
+def callback1():
     body = request.get_data(as_text=True)
     json_data = json.loads(body)
     print(json_data)
@@ -29,5 +57,4 @@ def callback():
     return 'OK'
 
 if __name__ == "__main__":
-    callback()
     app.run()
