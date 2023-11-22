9064474a2679cbb1621ffd323ebdfa8c9f666a15
ffloooot <149374300+ffloooot@users.noreply.github.com>
2023-11-22 23:33:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,22 +39,5 @@ def handle_message(event):
             TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
     return
 
-def callback1():
-    body = request.get_data(as_text=True)
-    json_data = json.loads(body)
-    print(json_data)
-    try:
-        line_bot_api = LineBotApi('IjD9cOGGINHUXelSEl+HdVAc9oEDw3/kk+XMkfWyGZCdFyURygI18eD4rKfcpaKxajwsLmA0iCwnedwrM/qPSCy5BcBNNw+z8xIx/k4ytwxrAABJspIvWUUTWEYZOnYGRUUtw1B9Ez2tyL9qhqWhcwdB04t89/1O/w1cDnyilFU=')
-        handler = WebhookHandler('6e4d6c59b5cd885348d5e5cc71a4957b')
-        signature = request.headers['X-Line-Signature']
-        handler.handle(body, signature)
-        tk = json_data['events'][0]['replyToken']         # 取得 reply token
-        msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
-        text_message = TextSendMessage(text=msg)          # 設定回傳同樣的訊息
-        line_bot_api.reply_message(tk,text_message)       # 回傳訊息
-    except:
-        print('error')
-    return 'OK'
-
 if __name__ == "__main__":
     app.run()
