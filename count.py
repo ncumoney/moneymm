b688fd0d6eebff8059a102afdd07e108c7f5e558
ffloooot <yunyunsu.1002@gmail.com>
2023-12-04 09:28:44 +0800

    少弄到了count進化

--- count.py
+++ count.py
@@ -1,35 +1,65 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-def count(category, data): ##data=使用者輸入的金額 category==類別
-    # 定義認證範圍
+def count(user_id, category, data):
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    # 添加您的 JSON 憑證文件
     creds = ServiceAccountCredentials.from_json_keyfile_name('steam-boulevard-405907-f1cc6b42920f.json', scope)
-    # 授權和建立客戶端
     client = gspread.authorize(creds)
     spreadsheet_name = "ncummmoney"
-    # 打開 spreadsheet
-    sheet = client.open(spreadsheet_name).sheet1
-    print([category, data])
+    sheet = client.open(spreadsheet_name)
+    worksheet_titles = [worksheet.title for worksheet in sheet.worksheets()]
+    worksheet_name_to_check = str(user_id)
 
-    # 插入數據
-    sheet.append_row([category, data])
-    allcount =sheet.col_values(2)
-    print(allcount)
+    if worksheet_name_to_check in worksheet_titles:
+        personsheet=sheet.worksheet(worksheet_name_to_check)
+    else:
+        personsheet = sheet.add_worksheet(title=worksheet_name_to_check, rows="1000", cols="1000")
+
+    personsheet.append_row([category, data])
+    allcount =personsheet.col_values(2)
     totocount = sum(float(value) for value in allcount if value)
-    print(totocount)
 
-    return totocount
-'''
-# 要添加的數據，例如 ["2023/03/01", "餐廳", 300]
-data =300 ##測試而已可刪==使用者輸入的金額 
-category="飲食" ##測試而已可刪==使用者輸入的類別
-# Spreadsheet 名稱
-spreadsheet_name = "ncummmoney" ###要放到main
+    maxxx=len(personsheet.col_values(1))
+    records = personsheet.col_values(1)
 
-# 呼叫函數添加數據
-count(spreadsheet_name, category, data) ####main
-'''
+    counttotal={}
+    for i in range(maxxx):
+      if records[i]=='日用品':
+        readwhere=int(personsheet.cell(i+1, 2).value)
+        if '日用品' in counttotal:
+          counttotal['日用品']+=readwhere
+        else:
+          counttotal['日用品']=readwhere
+        print(counttotal)
+      if records[i]=='娛樂':
+        readwhere=int(personsheet.cell(i+1, 2).value)
+        if '娛樂' in counttotal:
+          counttotal['娛樂']+=readwhere
+        else:
+          counttotal['娛樂']=readwhere
+        print(counttotal)
+      if records[i]=='交通':
+        readwhere=int(personsheet.cell(i+1, 2).value)
+        if '交通' in counttotal:
+          counttotal['交通']+=readwhere
+        else:
+          counttotal['交通']=readwhere
+        print(counttotal)
+      if records[i]=='飲食':
+        readwhere=int(personsheet.cell(i+1, 2).value)
+        if '飲食' in counttotal:
+          counttotal['飲食']+=readwhere
+        else:
+          counttotal['飲食']=readwhere
+        print(counttotal)
+    counttotal['餘額']=totocount
 
+    return counttotal
 
+data = 300
+user_id='U4249cbdf64ce421b9c8e225d9260db69'
+category='日用品'
+category_totals= count(user_id, category, data)
+reply_message = "各類別消費總額:\n" + "\n".join([f"{category}: {total}" for category, total in category_totals.items()])
+print(reply_message)
+#line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
