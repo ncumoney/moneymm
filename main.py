c13fc583e3a6b54695bed337c46a63b025086f25
moningter <914ctn@gmail.com>
2023-12-08 22:12:39 +0800

    問題

--- main.py
+++ main.py
@@ -336,7 +336,7 @@ if __name__ == "__main__":
     # Spreadsheet 名稱
     spreadsheet_name = "ncummmoney"
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)# 例外狀況
     # Configure the logging
     logging.basicConfig(level=logging.INFO)
 
