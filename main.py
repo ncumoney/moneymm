1d55fcb5e2682ea25930a370a79d16d55799ab4d
moningter <914ctn@gmail.com>
2023-12-12 10:13:56 +0800

    12/12

--- main.py
+++ main.py
@@ -337,7 +337,7 @@ if __name__ == "__main__":
     spreadsheet_name = "ncummmoney"
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)# 例外狀況
-    # Configure the logging
+    # Configure the logging、
     logging.basicConfig(level=logging.INFO)
 
     # Log statement
